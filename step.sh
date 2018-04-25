9b03de00cb077d769b5ac580c73dff05cecbc209
Richard Stelling <rich@lionheartapplications.com>
2018-04-25 15:05:13 +0100

    Added in ‘-‘ in <meta-data> element

--- step.sh
+++ step.sh
@@ -5,13 +5,13 @@
 # Test for xmlstarlet
 hash xmlstarlet 2>/dev/null || { echo >&2 "xmlstarlet required, but it's not installed."; exit 1; }
 
-EXISTING_VALUE=$(xmlstarlet sel -t -v "//metadata[@android:name='${metadata_name}']/@android:value" "${android_manifest_path}")
+EXISTING_VALUE=$(xmlstarlet sel -t -v "//meta-data[@android:name='${metadata_name}']/@android:value" "${android_manifest_path}")
 
 if [ ! -n "${EXISTING_VALUE}" ] # No Element, add it
 then
 
 echo
-echo "Added <metadata> to: ${android_manifest_path}"
+echo "Added <meta-data> to: ${android_manifest_path}"
 echo "\tName: ${metadata_name}"
 echo "\tValue: ${metadata_value}"
 echo
@@ -20,12 +20,12 @@ echo
 xmlstarlet ed --inplace \
 	-s "/manifest/application" \
 	-t elem \
-	-n "metadata" \
-	-i "/manifest/application/metadata[not(@android:name)]" \
+	-n "meta-data" \
+	-i "/manifest/application/meta-data[not(@android:name)]" \
 	-t attr \
 	-n "android:name" \
 	-v "${metadata_name}" \
-	-i "/manifest/application/metadata[not(@android:value)]" \
+	-i "/manifest/application/meta-data[not(@android:value)]" \
 	-t attr \
 	-n "android:value" \
 	-v "${metadata_value}" "${android_manifest_path}"
@@ -34,13 +34,13 @@ elif [ $EXISTING_VALUE != ${metadata_value} ] # Found element, update it
 then
 
 echo
-echo "Updating <metadata> at: ${android_manifest_path}"
+echo "Updating <meta-data> at: ${android_manifest_path}"
 echo "\tName: ${metadata_name}"
 echo "\tValue: ${metadata_value}"
 echo
 
 xmlstarlet ed --inplace \
-	-u "/manifest/application/metadata[@android:name='${metadata_name}']/@android:value" \
+	-u "/manifest/application/meta-data[@android:name='${metadata_name}']/@android:value" \
 	-v "${metadata_value}" "${android_manifest_path}"
 
 else
